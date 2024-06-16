4b296afbbf62608d844bec4abb3effd965f30ccd
PetrVorlicek <vorlicek.petricek@gmail.com>
2024-06-16 22:11:31 +0200

    feat: add items detail

--- backend/app/eshop/views.py
+++ backend/app/eshop/views.py
@@ -1,7 +1,21 @@
+from typing import Any
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from eshop.models import Item
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, "index.html")
+    return render(
+        request,
+        "index.html",
+    )
+
+
+class ItemView(DetailView):
+    model = Item
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        return context
